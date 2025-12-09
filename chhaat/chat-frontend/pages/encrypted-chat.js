7d29c89d9fe3f2d75619dcdfdaf5cdab68383951
Varun3344 <varungowda485@gmail.com>
2025-12-09 16:05:11 +0530

    fully updated

--- chhaat/chat-frontend/pages/encrypted-chat.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Chat = dynamic(() => import("../components/Chat"), { ssr: false });
-
-export default function EncryptedChatPage() {
-  return <Chat />;
-}
